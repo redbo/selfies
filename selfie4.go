e2dee843d8b4fb4c954132c9c55030b75d341cdc
Mike Barton <mike@weirdlooking.com>
2019-07-23 23:57:02 +0100

    text rendering

--- selfie4.go
+++ selfie4.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/draw"
 	"io/ioutil"
 	"log"
 	"os"
@@ -16,33 +15,6 @@ import (
 	"golang.org/x/image/font"
 	"golang.org/x/image/math/fixed"
 )
-
-func textImage(text string) *image.RGBA {
-	fontBytes, err := ioutil.ReadFile("Raleway-Black.ttf")
-	if err != nil {
-		log.Fatalf("failed to read fong: %v", err)
-	}
-	f, err := truetype.Parse(fontBytes)
-	if err != nil {
-		log.Fatalf("failed to parse font: %v", err)
-	}
-	im := image.NewRGBA(image.Rect(0, 0, 900, 1600))
-	draw.Draw(im, im.Bounds(), image.Transparent, image.ZP, draw.Src)
-	d := &font.Drawer{
-		Dst: im,
-		Src: image.White,
-		Face: truetype.NewFace(f, &truetype.Options{
-			Size: 100,
-			DPI:  300,
-		}),
-	}
-	d.Dot = fixed.Point26_6{
-		X: (fixed.I(900) - d.MeasureString(text)) / 2,
-		Y: fixed.I(800),
-	}
-	d.DrawString(text)
-	return im
-}
 
 func main() {
 	os.Setenv("DISPLAY", ":0")
@@ -83,6 +55,28 @@ func main() {
 		log.Fatalf("failed to create surface: %v", err)
 	}
 
+	fontBytes, err := ioutil.ReadFile("Raleway-Black.ttf")
+	if err != nil {
+		log.Fatalf("failed to read fong: %v", err)
+	}
+	parsedFont, err := truetype.Parse(fontBytes)
+	if err != nil {
+		log.Fatalf("failed to parse font: %v", err)
+	}
+	im := &image.RGBA{
+		Pix:    surface.Pixels(),
+		Stride: 900 * 4,
+		Rect:   image.Rect(0, 0, 900, 1600),
+	}
+	d := &font.Drawer{
+		Dst: im,
+		Src: image.White,
+		Face: truetype.NewFace(parsedFont, &truetype.Options{
+			Size: 500,
+			DPI:  100,
+		}),
+	}
+
 	cam, err := webcam.Open("/dev/video0")
 	if err != nil {
 		log.Fatalf("failed to open video device: %v", err)
@@ -101,17 +95,6 @@ func main() {
 		log.Fatalf("failed to begin cam streaming: %v", err)
 	}
 	cam.SetBufferCount(1)
-
-	text3 := textImage("3")
-	textSurface, err := sdl.CreateRGBSurface(0, 900, 1600, 32, 0, 0, 0, 0)
-	if err != nil {
-		log.Fatalf("error making text surface: %v", err)
-	}
-	dp := textSurface.Pixels()
-	for i := range dp {
-		dp[i] = byte(text3.Pix[i])
-	}
-	copy(dp, text3.Pix)
 
 	for {
 		t := time.Now()
@@ -136,7 +119,11 @@ func main() {
 				break
 			}
 		}
-		textSurface.Blit(&sdl.Rect{X: 0, Y: 0, W: 900, H: 1600}, surface, &sdl.Rect{X: 0, Y: 0, W: 900, H: 1600})
+		d.Dot = fixed.Point26_6{
+			X: (fixed.I(900) - d.MeasureString("3")) / 2,
+			Y: fixed.I(800),
+		}
+		d.DrawString("3")
 		window.UpdateSurface()
 		fmt.Println(time.Since(t))
 	}
